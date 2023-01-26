090850004f7ee5c8341b8dd65401074a9ed4c958
isac moreira <108782511+isacmoreira10@users.noreply.github.com>
2023-01-26 14:39:31 -0300

    Update script.js

--- counter2/script.js
+++ counter2/script.js
@@ -3,21 +3,21 @@ var count = 0;
 
 
 
-function show(){
+  function show(){
     var output = document.querySelector('#output');
     var number = document.querySelector( '#number' ).value;
         number = parseFloat(number);
     numbers.push(number);  
                  document.querySelector( '#number' ).value = '';
                  output.innerHTML = numbers.join('+');                
-}
+  }
 
 
 
-function calculate(){
+  function calculate(){
     var output = document.querySelector( '#output' );
-for(var i =0;i<numbers.length;i++){
+  for(var i =0;i<numbers.length;i++){
     count += numbers[i];
     output.innerHTML =  numbers.join('+') + '=' + count;
-}
-}
+  }
+  }
